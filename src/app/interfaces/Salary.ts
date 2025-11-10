2b5433f7e0d8c441141c43c6f78f043fca9e915c
Yennyfer Andrea Garcia Forney <y.garcia.forney@accenture.com>
2025-11-10 00:31:28 -0300

    Add salary form functionality with dynamic selections and submission handling

---
+++ src/app/interfaces/Salary.ts
@@ -0,0 +1,8 @@
+export interface Salary {
+  id: number;
+  area: string | null;
+  location: string | null;
+  position: string | null;
+  experienceLevel: string | null;
+  salary: number | null;
+}
