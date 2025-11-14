0add5b791be12ddbcd873b54733da2ef6808252f
richarddlb <richardlopezbo.ar@gmail.com>
2025-11-14 18:18:16 -0300

    se termina de organizar el formulario de la pagina salario

--- src/app/pages/Salaries/Salaries.ts
+++ src/app/pages/Salaries/Salaries.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { InquiryForm } from '../../components/inquiry-form/inquiry-form';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-salaries',
   standalone: true,
-  imports: [InquiryForm],
+  imports: [InquiryForm, RouterLink, RouterLinkActive],
   templateUrl: './Salaries.html',
   styleUrl: './Salaries.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
