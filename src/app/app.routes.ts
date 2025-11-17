27701481d4aa992290411c49df58d328dfa46512
oscar_roth <oscarroth@gmail.com>
2025-11-17 11:01:19 -0300

    pantalla administrador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Salaries } from './pages/Salaries/Salaries';
 import { Login } from './pages/Login/Login';
 import { SignIn } from './pages/Sign-in/Sign-in';
 import { CreateSalary } from './pages/Create-salary/Create-salary';
+import { AdministratorComponent } from './pages/Administrator/Administrator';
 
 export const routes: Routes = [
   {
@@ -20,6 +21,10 @@ export const routes: Routes = [
   {
     path: 'create-salary',
     component: CreateSalary
+  },
+  {
+    path: 'admin',
+    component: AdministratorComponent
   },
   {
     path: '**',
